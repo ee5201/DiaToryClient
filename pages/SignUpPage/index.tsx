e76b5b5b17eb80f99b62973ee290b6faf2044cef
kyungmin kim <ee5201@naver.com>
2023-07-21 01:32:47 +0900

    Feat SignPage추가

---
+++ pages/SignUpPage/index.tsx
@@ -0,0 +1,10 @@
+import SignUpPageContainer from "../../src/components/units/SignUpPage/SignUpPage.container";
+
+export default function SignUpPage() {
+  return (
+    //
+    <>
+      <SignUpPageContainer />
+    </>
+  );
+}
